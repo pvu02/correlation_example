909ce5d029d182d4eeb790acf3551351a1414875
Janie Vu <Janie Vu>
2016-10-04 11:02:48 -0400

    Third commit.

--- correlation_example_janie.R
+++ correlation_example_janie.R
@@ -25,3 +25,41 @@ cor.test(x=analytic.data$extrav,y=analytic.data$popular)
 
 # Report r=.32, 95% CI [.28, .35], p < .001
 
+# Understanding how  R returns information
+my.cor.result <- cor.test(x=analytic.data$extrav,y=analytic.data$popular)
+my.cor.result
+
+#  See structure of resulting information
+str(my.cor.result)
+
+# Get correlation
+my.cor.result$estimate
+
+# Plot single correlation
+my.scatter <- qplot(x=extrav,y=popular,data=analytic.data)
+print(my.scatter)
+
+# Adding regression line (with confidence intervals; se = FALSE means no confidence intervals)
+my.scatter <- qplot(x=extrav,y=popular,data=analytic.data)
+my.scatter <- my.scatter + geom_smooth(method = "lm", se = TRUE, color="black")
+print(my.scatter)
+
+# See all options from geom_smooth
+?geom_smooth
+
+# Calculating several correlations - converting data set to data frame
+library(apaTables)
+apa.cor.table(as.data.frame(analytic.data))
+
+# Plots of several correlations (lm = TRUE means regression line added)
+psych::pairs.panels(as.data.frame(analytic.data),lm=TRUE)
+
+# Subgroup analyses - Calculating a correlation for a single subgroup
+analytic.data.class.is.5 <- filter(analytic.data, class==5)
+View(analytic.data.class.is.5)
+cor.test(x=analytic.data.class.is.5$extrav,y=analytic.data.class.is.5$popular)
+
+# Calculating correlations for several subgroups
+analytic.data.grouped.by.class <- group_by(analytic.data, class)
+cor.by.group <- summarise(analytic.data.grouped.by.class, cor.test(x=extrav,y=popular)$estimate)
+print(cor.by.group)
